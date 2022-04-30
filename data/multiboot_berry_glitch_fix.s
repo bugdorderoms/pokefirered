188b5a61931172d76c09ffc5875fa5a70f7203fb
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-30 20:28:11 -0300

    Add files via upload

---
+++ data/multiboot_berry_glitch_fix.s
@@ -0,0 +1,5 @@
+	.section .rodata
+
+gMultiBootProgram_BerryGlitchFix_Start:: @ 86FFC6C
+	.incbin "data/mb_berry_fix.gba"
+gMultiBootProgram_BerryGlitchFix_End::
---
+++ data/multiboot_pokemon_colosseum.s
@@ -0,0 +1,5 @@
+	.section .rodata
+
+gMultiBootProgram_PokemonColosseum_Start:: @ 8703860
+	.incbin "data/mb_colosseum.gba"
+gMultiBootProgram_PokemonColosseum_End::
