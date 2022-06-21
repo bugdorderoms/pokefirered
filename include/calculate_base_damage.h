9d62240c82758c5d05bbc2f7760b171ae1c6fca8
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-06-21 10:42:24 -0300

    Update calculate_base_damage.h

--- include/calculate_base_damage.h
+++ include/calculate_base_damage.h
@@ -3,6 +3,6 @@
 
 #include "global.h"
 
-s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *defender, u32 move, u16 sideStatus, u16 powerOverride, bool8 isConfusionDmg, u8 bankAtk, u8 bankDef);
+s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *defender, u32 move, u16 sideStatus, u16 powerOverride, bool8 isConfusionDmg, u8 battlerIdAtk, u8 battlerIdDef);
 
 #endif // GUARD_CALCULATE_BASE_DAMAGE_H
