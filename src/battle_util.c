43cdc5bc8dbb0b981da1ff0224bb2db402e17342
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-06-23 09:00:22 -0300

    Update battle_util.c

--- src/battle_util.c
+++ src/battle_util.c
@@ -2318,7 +2318,7 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
 			    break;
 		    case ABILITY_ANGER_POINT:
 			    if (!(gMoveResultFlags & MOVE_RESULT_NO_EFFECT) && TARGET_TURN_DAMAGED && gCritMultiplier == 2
-				&& gBattleMons[gBattlerTarget].hp != 0 && !(gBattleMons[gBattlerTarget].status2 & STATUS2_SUBSTITUTE)
+				&& gBattleMons[gBattlerTarget].hp != 0 && !SubsBlockMove(gBattlerAttacker, gBattlerTarget, moveArg)
 				&& gBattleMons[gBattlerTarget].statStages[STAT_ATK] < 0xC)
 			    {
 				    gBattleMons[gBattlerTarget].statStages[STAT_ATK] = 0xC;
@@ -2363,6 +2363,27 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
 				    ++effect;
 			    }
 			    break;
+	    }
+	    break;
+	case ABILITYEFFECT_MOVE_END_ATTACKER:
+	    if (!(gMoveResultFlags & MOVE_RESULT_NO_EFFECT) && gBattleMons[gBattlerTarget].hp != 0 && !SubsBlockMove(gBattlerAttacker, gBattlerTarget, moveArg)
+		&& TARGET_TURN_DAMAGED)
+	    {
+		    switch (gLastUsedAbility)
+		    {
+			    case ABILITY_POISON_TOUCH:
+				    if (!gProtectStructs[gBattlerTarget].confusionSelfDmg && (gBattleMoves[moveArg].flags & FLAG_MAKES_CONTACT) && (Random() % 3) == 0
+				       && CanBePoisoned(gBattlerTarget, gBattlerAttacker) && gBattleMons[gBattlerTarget].ability != ABILITY_SHIELD_DUST)
+				    {
+					    gBattleCommunication[MOVE_EFFECT_BYTE] = MOVE_EFFECT_POISON;
+					    PREPARE_ABILITY_BUFFER(gBattleTextBuff1, gLastUsedAbility);
+					    BattleScriptPushCursor();
+					    gBattlescriptCurrInstr = BattleScript_ApplySecondaryEffect;
+					    gHitMarker |= HITMARKER_IGNORE_SAFEGUARD;
+					    ++effect;
+				    }
+				    break;
+		    }
 	    }
 	    break;
         case ABILITYEFFECT_IMMUNITY: // 5
