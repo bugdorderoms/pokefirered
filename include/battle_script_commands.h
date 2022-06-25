83cfb970737ea4e1494da212fae58005ccfcc95f
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-06-25 18:28:40 -0300

    Update battle_script_commands.h

--- include/battle_script_commands.h
+++ include/battle_script_commands.h
@@ -12,7 +12,7 @@
 void SetMoveEffect(bool8 primary, u8 certain);
 bool8 UproarWakeUpCheck(u8 battlerId);
 u8 AI_TypeCalc(u16 move, u16 targetSpecies, u16 targetAbility);
-u8 TypeCalc(u16 move, u8 attacker, u8 defender);
+u8 TypeCalc(u16 move, u8 attacker, u8 defender, bool8 recordAbilities);
 void AI_CalcDmg(u8 attacker, u8 defender);
 bool32 IsMonGettingExpSentOut(void);
 void BattleCreateYesNoCursorAt(void);
