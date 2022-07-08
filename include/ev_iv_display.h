26aca4a286a174a79c5fceed039b86e3b400cf70
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-07-08 09:38:03 -0300

    Add files via upload

---
+++ include/ev_iv_display.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_EV_IV_DISPLAY_H
+#define GUARD_EV_IV_DISPLAY_H
+
+#define HIDDEN_POWER_TYPE TRUE // if true it show the hidden power type based on the mon Iv's
+#define PIKACHU_ICON TRUE // if true it creates an pikachu's icon at the top of the screen
+
+void ShowEvIvDisplay(MainCallback exitCallback);
+
+#endif // GUARD_EV_IV_DISPLAY_H
