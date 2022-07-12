429216585accc676ed6517efeb46c76d4fc8c95d
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-07-12 11:09:49 -0300

    Update dns.h

--- include/dns.h
+++ include/dns.h
@@ -2,9 +2,17 @@
 #define GUARD_DNS_UTILS_H
 
 #define USE_DNS_IN_BATTLE   TRUE
+#define LIT_UP_WINDOWS      TRUE
 
 #define DNS_PAL_EXCEPTION   FALSE
 #define DNS_PAL_ACTIVE      TRUE
+
+struct lightingColour
+{
+    u8 paletteNum;
+    u8 colourNum;
+    u16 lightColour;
+};
 
 struct DNSPalExceptions 
 {
