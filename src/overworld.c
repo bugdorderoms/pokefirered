b91f62a39ded5a3edf784db91d4cfee2e6e506fc
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-07-13 12:11:28 -0300

    Update overworld.c

--- src/overworld.c
+++ src/overworld.c
@@ -2102,11 +2102,7 @@ static void ResumeMap(u32 a1)
     ResetAllPicSprites();
     ResetCameraUpdateInfo();
     InstallCameraPanAheadCallback();
-    if (!a1)
-        InitObjectEventPalettes(0);
-    else
-        InitObjectEventPalettes(1);
-
+    FreeAllSpritePalettes();
     FieldEffectActiveListClear();
     StartWeather();
     ResumePausedWeather();
