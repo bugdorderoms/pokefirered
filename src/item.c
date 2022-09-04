4f8ccb9c262d73db938ac741b5c54609c6043482
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-09-04 09:41:45 -0300

    Update item.c

--- src/item.c
+++ src/item.c
@@ -689,11 +689,11 @@ bool8 GetSetItemObtained(u16 item, u8 caseId)
 	
     switch (caseId)
     {
-		case FLAG_GET_OBTAINED:
-		    return gSaveBlock2Ptr->itemFlags[index] & mask;
-		case FLAG_SET_OBTAINED:
-			gSaveBlock2Ptr->itemFlags[index] |= mask;
-			return TRUE;
+	    case FLAG_GET_OBTAINED:
+		return gSaveBlock2Ptr->itemFlags[index] & mask;
+	    case FLAG_SET_OBTAINED:
+		gSaveBlock2Ptr->itemFlags[index] |= mask;
+		return TRUE;
     }
     return FALSE;
 }
