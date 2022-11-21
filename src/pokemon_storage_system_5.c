75bafaaa16a838047c4e3a6d871e0ac07f68e712
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-11-21 11:43:16 -0300

    Update pokemon_storage_system_5.c

--- src/pokemon_storage_system_5.c
+++ src/pokemon_storage_system_5.c
@@ -1026,7 +1026,6 @@ static void SetCursorMonData(void *pokemon, u8 mode)
                 gPSSData->cursorMonIsEgg = GetMonData(mon, MON_DATA_IS_EGG);
 
             GetMonData(mon, MON_DATA_NICKNAME, gPSSData->cursorMonNick);
-            StringGetEnd10(gPSSData->cursorMonNick);
             gPSSData->cursorMonLevel = GetMonData(mon, MON_DATA_LEVEL);
             gPSSData->cursorMonMarkings = GetMonData(mon, MON_DATA_MARKINGS);
             gPSSData->cursorMonPersonality = GetMonData(mon, MON_DATA_PERSONALITY);
@@ -1051,7 +1050,6 @@ static void SetCursorMonData(void *pokemon, u8 mode)
 
 
             GetBoxMonData(boxMon, MON_DATA_NICKNAME, gPSSData->cursorMonNick);
-            StringGetEnd10(gPSSData->cursorMonNick);
             gPSSData->cursorMonLevel = GetLevelFromBoxMonExp(boxMon);
             gPSSData->cursorMonMarkings = GetBoxMonData(boxMon, MON_DATA_MARKINGS);
             gPSSData->cursorMonPersonality = GetBoxMonData(boxMon, MON_DATA_PERSONALITY);
