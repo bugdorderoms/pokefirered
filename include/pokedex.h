e0b1fa50f3b6e8ad94011fddfc0205aa432d3429
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-12-23 09:47:02 -0300

    Update pokedex.h

--- include/pokedex.h
+++ include/pokedex.h
@@ -1,10 +1,6 @@
 #ifndef GUARD_POKEDEX_H
 #define GUARD_POKEDEX_H
 
-#define KANTO_DEX_COUNT 151
-#define HOENN_DEX_COUNT 202
-#define NATIONAL_DEX_COUNT 386
-
 enum
 {
     DEX_MODE_KANTO,
@@ -38,12 +34,10 @@ void CopyMonCategoryText(u16 species, u8 *dst);
 u16 GetPokedexHeightWeight(u16 dexNum, u8 data);
 u16 GetNationalPokedexCount(u8);
 u16 GetKantoPokedexCount(u8);
-bool16 HasAllMons(void);
 u8 CreateDexDisplayMonDataTask(u16 dexNum, u32 trainerId, u32 personality);
 s8 GetSetPokedexFlag(u16 nationalNum, u8 caseId);
 u16 GetNationalPokedexCount(u8);
 u16 GetKantoPokedexCount(u8);
-bool16 HasAllHoennMons(void);
 bool16 HasAllKantoMons(void);
 bool16 HasAllMons(void);
 
