51d45e7d164509c013f910707aa4ff95ed58f4c4
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-12-31 15:18:36 -0300

    Update rtc_events.c

--- src/rtc_events.c
+++ src/rtc_events.c
@@ -9,25 +9,25 @@ static void DoPerMinuteBasedEvents(void);
 
 void RtcUpdatePerMonthEvents(u8 thisMonth, u8 lastMonth)
 {
-	if (thisMonth > lastMonth || (thisMonth == 1 && thisMonth < lastMonth))
+	if (thisMonth != lastMonth)
 		DoPerMonthBasedEvents();
 }
 
 void RtcUpdatePerDayEvents(u8 thisDay, u8 yesterDay)
 {
-	if (thisDay > yesterDay || (thisDay == 1 && thisDay < yesterDay))
+	if (thisDay != yesterDay)
 		DoPerDayBasedEvents();
 }
 
 void RtcUpdatePerHourEvents(u8 thisHour, u8 lastHour)
 {
-	if (thisHour > lastHour || (thisHour == 0 && thisHour < lastHour))
+	if (thisHour != lastHour)
 		DoPerHourBasedEvents();
 }
 
 void RtcUpdatePerMinuteEvents(u8 thisMinute, u8 lastMinute)
 {
-	if (thisMinute > lastMinute || (thisMinute == 0 && thisMinute < lastMinute))
+	if (thisMinute != lastMinute)
 		DoPerMinuteBasedEvents();
 }
 
