c8670546ca91af9de5f809bc06dfd50d253171d6
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2023-01-04 10:55:42 -0300

    Update field_player_avatar.c

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -513,40 +513,40 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
 
     if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
     {
-		if (IsDexNavSearchActive() && (heldKeys & A_BUTTON))
-		{
-			gPlayerAvatar.creeping = TRUE;
-			PlayerGoSlow(direction);
-		}
-		else // speed 2 is fast, same speed as running
-		    PlayerGoSpeed2(direction);
+	if (IsDexNavSearchActive() && (heldKeys & A_BUTTON))
+	{
+	    gPlayerAvatar.creeping = TRUE;
+	    PlayerGoSlow(direction);
+	}
+	else // speed 2 is fast, same speed as running
+	    PlayerGoSpeed2(direction);
         return;
     }
 
     if ((heldKeys & B_BUTTON || gSaveBlock2Ptr->autoRun) && FlagGet(FLAG_SYS_B_DASH) && !IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior))
     {
         if (PlayerIsMovingOnRockStairs(direction))
-		{
-			if (heldKeys & B_BUTTON && gSaveBlock2Ptr->autoRun)
-				PlayerGoSlow(direction);
-			else
-				PlayerRunSlow(direction);
-		}
+	{
+		if (heldKeys & B_BUTTON && gSaveBlock2Ptr->autoRun)
+			PlayerGoSlow(direction);
+		else
+			PlayerRunSlow(direction);
+	}
         else
-		{
-			if (heldKeys & B_BUTTON && gSaveBlock2Ptr->autoRun)
-				PlayerGoSpeed1(direction);
-			else
-				PlayerRun(direction);
-		}
+	{
+		if (heldKeys & B_BUTTON && gSaveBlock2Ptr->autoRun)
+			PlayerGoSpeed1(direction);
+		else
+			PlayerRun(direction);
+	}
         gPlayerAvatar.flags |= PLAYER_AVATAR_FLAG_DASH;
         return;
     }
-	else if (IsDexNavSearchActive() && (heldKeys & A_BUTTON))
-	{
-		gPlayerAvatar.creeping = TRUE;
-		PlayerGoSlow(direction);
-	}
+    else if (IsDexNavSearchActive() && (heldKeys & A_BUTTON))
+    {
+	    gPlayerAvatar.creeping = TRUE;
+	    PlayerGoSlow(direction);
+    }
     else
     {
         if (PlayerIsMovingOnRockStairs(direction))
